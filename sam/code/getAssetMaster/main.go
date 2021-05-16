26e5d86e581a6b9d2eb343057fa79c8d463270d7
wtnb-s <denebu.1.4@gmail.com>
2021-05-17 00:12:44 +0900

    init project

---
+++ sam/code/getAssetMaster/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// 環境変数設定
+	endpoint := os.Getenv("DYNAMODB_ENDPOINT")
+	// パスパラメータ取得
+	assetCode := request.PathParameters["assetCode"]
+
+	// Dynamodb接続設定
+	session := session.Must(session.NewSession())
+	config := aws.NewConfig().WithRegion("ap-northeast-1")
+	if len(endpoint) > 0 {
+		config = config.WithEndpoint(endpoint)
+		fmt.Print(endpoint)
+	}
+	db := dynamodb.New(session, config)
+
+	param, err := db.Query(&dynamodb.QueryInput{
+		TableName: aws.String("asset_master"),
+		ExpressionAttributeNames: map[string]*string{
+			"#AssetCode":  aws.String("AssetCode"),
+			"#CategoryId": aws.String("CategoryId"),
+			"#Name":       aws.String("Name"),
+			"#Type":       aws.String("Type"),
+		},
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":assetCode": {
+				S: aws.String(assetCode),
+			},
+		},
+		KeyConditionExpression: aws.String("#AssetCode=:assetCode"),
+		ProjectionExpression:   aws.String("#AssetCode, #CategoryId, #Name, #Type"),
+	})
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	assetMaster := make([]*AssetMasterRes, 0)
+	if err := dynamodbattribute.UnmarshalListOfMaps(param.Items, &assetMaster); err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	jsonBytes, _ := json.Marshal(assetMaster)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(jsonBytes),
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+type AssetMasterRes struct {
+	AssetCode  string `json:"AssetCode"`
+	CategoryId int    `json:"CategoryId"`
+	Name       string `json:"Name"`
+	Type       int    `json:"Type"`
+}
---
+++ sam/code/go.mod
@@ -0,0 +1,11 @@
+module app
+
+go 1.15
+
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/aws/aws-lambda-go v1.23.0
+	github.com/aws/aws-sdk-go v1.38.40
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca
+	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
+)
